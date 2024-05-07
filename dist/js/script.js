c03388d449ae1f81f916a2e68d060ec534a9b76a
amar asfaw <amarasfaw1077@gmail.com>
2024-05-07 22:55:06 +0300

    add about section witcss style

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,16 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            padding: '16px', 
+        },
+        extend: {
+            screens: {
+                '2xl': '1230px',
+            }
+        }  
+    }
+}
